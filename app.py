43b54d840cd5cd136a38a9de82b2c7e55a63b351
Chris Beckert <ccbeckert@gmail.com>
2022-05-15 01:57:15 +0200

    hello world

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/hello")
+def hello_world():
+    return "<h1>Hallo wereld</h1>"
+
+if __name__ == "__main__":
+    app.run()
