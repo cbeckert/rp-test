aecfffcf656a0bde1266e5f8955ab69d4488acaf
Chris Beckert <ccbeckert@gmail.com>
2022-05-15 12:57:15 +0200

    flask-hello-world

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/hello")
 def hello_world():
-    return "<h1>Hallo wereld</h1>"
+    return "<h1 style='color:blue;'>Hallo wereld</h1>"
 
 if __name__ == "__main__":
     app.run()
